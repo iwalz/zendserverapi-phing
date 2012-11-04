d18ea5170d2bc1889f5fbe45008ed4df817f9523
ingo <ingo.walz@googlemail.com>
2012-11-04 21:59:30 +0100

    Fixed server attribute
    Change-Id: Ib52963c13e28ec55390720421c104c668c7d27e4

--- src/Phing/ZSApiTask.php
+++ src/Phing/ZSApiTask.php
@@ -1,11 +1,33 @@
 <?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * <http://www.rubber-duckling.net>
+ */
 require_once 'phing/Task.php';
 require_once 'vendor/composer/autoload_classmap.php';
 require_once 'vendor/autoload.php';
 
+/**
+ * ApplicationUpdate phing task.
+ *
+ * @license     MIT
+ * @link        http://github.com/iwalz/zendserverapi-phing
+ * @author      Ingo Walz <ingo.walz@googlemail.com>
+ */
 abstract class ZSApiTask extends Task
 {
-    protected $name = null;
+    protected $server = null;
     protected $returnProperty = null;
     
     public function setReturnProperty($returnProperty) {
@@ -16,12 +38,12 @@ abstract class ZSApiTask extends Task
         return $this->returnProperty;
     }
     
-    public function setName($name) {
-        $this->name = $name;
+    public function setServer($server) {
+        $this->server = $server;
     }
     
-    public function getName() {
-        return $this->name;
+    public function getServer() {
+        return $this->server;
     }
     
     public function init() 
@@ -36,9 +58,5 @@ abstract class ZSApiTask extends Task
         }
     }
     
-    private function getArrayForDataType($dataType) {
-        $rootKey = get_class($dataType);
-        $rootArray = $dataType->getArray();
-    }
 }
 ?>
